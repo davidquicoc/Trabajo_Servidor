12e00535e1ede7ece165523440cac78254da7df8
davidquicoc <davidquicoc@gmail.com>
2025-11-07 18:08:56 +0100

    Actualización de ficheros y config.php para conexión a bd

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
 session_start();
+require_once 'config.php';
+
+//  Comprobar conexión a BD "Trabajo_Servidor"
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+        } else {
+            $_SESSION['login-error'] = "Contraseña incorrecta.";
+        }
+    } else {
+        $_SESSION['login-error'] = "Correo no encontrado.";
+    }
+} else {
+    $_SESSION['login-error'] = "Método de envío no correspondiente.";
+}
 
 if (isset($_SESSION['name'])) {
     header("Location: productos.php");
     exit();
 }
+
 
 $error = $_SESSION['login-error'] ?? '';
 
@@ -13,8 +35,8 @@ session_unset();
 function errorLogin($error) {
     return !empty($error) ? "<p>$error</p>" : '';
 }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -36,6 +58,7 @@ function errorLogin($error) {
                 <input type="password" name="password" id="password" placeholder="Contraseña" required>
                 <button type="submit">Enviar</button>
             </form>
+            <p>No tienes una cuenta. <a href="register.php">Registrate aquí</a></p>
         </div>
     </div>
 
--- login-confirm.php
+++ login-confirm.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-require_once 'conexion.php';
+require_once 'config.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $result = $conn->query("SELECT * FROM ");
 }
 ?>
